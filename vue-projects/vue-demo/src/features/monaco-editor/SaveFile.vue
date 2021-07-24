f75870c94dadc3bc90e1277b45a94b83880c1057
Eason Yi <lisliefor@163.com>
2021-07-24 22:17:47 +0800

    finish my first widget

---
+++ vue-projects/vue-demo/src/features/monaco-editor/SaveFile.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'SaveFile',
+  data () {
+    return {
+      msg: 'Welcome to Your first widget'
+    }
+  }
+}
+</script>
--- vue-projects/vue-demo/src/router/index.js
+++ vue-projects/vue-demo/src/router/index.js
@@ -1,11 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import SaveFile from '@/features/monaco-editor/SaveFile'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/save-file',
+      name: 'Save File',
+      component: SaveFile
+    },
     {
       path: '/',
       name: 'HelloWorld',
