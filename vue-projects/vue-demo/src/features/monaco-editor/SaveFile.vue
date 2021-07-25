cbaeae4e82dd658df1b82cf26ecddc27902fd2d0
Eason Yi <lisliefor@163.com>
2021-07-25 10:35:49 +0800

    至此mocano-editor

--- vue-projects/vue-demo/src/features/monaco-editor/SaveFile.vue
+++ vue-projects/vue-demo/src/features/monaco-editor/SaveFile.vue
@@ -1,13 +1,48 @@
 <template>
-  <h1>{{ msg }}</h1>
+  <div>
+    <h1>{{ msg }}</h1>
+    <div ref="container" style="height: 360px; width: 100%;text-align: left;"></div>
+    <button @click="saveFile()" type="button">保存文件</button>
+  </div>
 </template>
 
 <script>
+import * as monaco from 'monaco-editor'
 export default {
   name: 'SaveFile',
   data () {
     return {
       msg: 'Welcome to Your first widget'
+    }
+  },
+  mounted () {
+    // 初始化编辑器，确保dom已经渲染，dialog中要写在opened中
+    this.monacoEditor = monaco.editor.create(this.$refs.container, {
+      value: '内容',
+      readOnly: false,
+      language: 'javascript',
+      theme: 'vs-dark',
+      selectOnLineNumbers: true,
+      roundedSelection: false,
+      cursorStyle: 'line',
+      automaticLayout: false,
+      glyphMargin: true,
+      useTabStops: false,
+      fontSize: 14,
+      autoIndent: true,
+      quickSuggestionsDelay: 500
+    })
+  },
+  methods: {
+    changeEditor (result) {
+      this.monacoEditor.setValue(result.data)
+      this.monacoEditor.getAction('editor.action.formatDocument')._run()
+    },
+    destroyEditor () {
+      this.monacoEditor.dispose()
+    },
+    saveFile: function (evt) {
+      console.log(evt)
     }
   }
 }
